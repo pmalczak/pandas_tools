e1bc59062cfadc64657037a16fb2f1317934836c
piotr <piotr.malczak@fingo.pl>
2024-10-20 13:15:36 +0200

    source code arranged to stick with weels std

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pandas-tools",
+    version="0.3.0",
+    author="Piotr Malczak",
+    author_email="pmalczak@gmail.com",
+    description="",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.10',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU GPL-v3',
+        'Operating System :: OS Independent',
+    ],
+)
